534e3a7d5d1bb8bf591d85412d86f83f753d4e1c
Marcelo H. Filho <contato@omarceleza.com.br>
2020-08-25 22:28:34 -0300

    Add arquivo compactado com o gif de modelo.

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -12,18 +12,15 @@ class HomeController extends Controller
     public function index()
     {
       $frames = [];
-      // dd($frames);
 
       foreach (Storage::allFiles('public/gifFrames') as $frame) {
         $frames[] = imagecreatefrompng(storage_path('app/' . $frame));
       }
 
-      // dd(imagecreatefromstring(storage_path($frames[0])));
-      // dd($frames);
       $maker = new GifCreator();
       $maker->create($frames, 250, 0);
       header('Content-type: image/gif');
-      header('Content-Disposition: filename="butterfly.gif"');
+      header('Content-Disposition: filename="bicuda-no-bozo.gif"');
       echo $maker->getGif();
     }
 }
